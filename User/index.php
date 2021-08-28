0173dd57e993692a876f88e5f8422ae547e54694
MiNi <nitipoom42@gmail.com>
2021-08-29 01:45:19 +0700

    -

--- User/index.php
+++ User/index.php
@@ -44,7 +44,6 @@
         <?php require_once('navbar.php'); ?>
         <br>
         <br>
-
         <!-- โปรโมชั่น -->
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-indicators">
