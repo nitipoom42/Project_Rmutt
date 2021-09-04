90420d518c93e032ec45d87a5da98d9a0280b787
MiNi <nitipoom42@gmail.com>
2021-09-04 23:01:50 +0700

    ทำรายการสั่งซื้อหน้าร้าน

--- sql/db_admin_confirm_pay.php
+++ sql/db_admin_confirm_pay.php
@@ -8,12 +8,8 @@ if (isset($_POST['admin_confirm_pay'])) {
     $data_pay = [
         'ID_Oder' => $_POST['ID_Oder'],
         'ID_Member' => $_POST['ID_Member'],
-
         'oder_status' => 2,
     ];
-
-    result($data_pay);
-
     $sql_oder = "UPDATE oder SET oder_status=:oder_status WHERE ID_Oder=:ID_Oder AND ID_Member=:ID_Member  ";
     $stmt_oder = $conn->prepare($sql_oder);
     $stmt_oder->execute($data_pay);
