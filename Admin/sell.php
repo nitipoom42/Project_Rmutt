a9d9b870ce6630d12abf2531716b21c220fe1131
MiNi <nitipoom42@gmail.com>
2021-09-06 00:14:55 +0700

    ปรับปรุง การโหลดหน้า sell

--- Admin/sell.php
+++ Admin/sell.php
@@ -59,83 +59,15 @@ $result_cart_sell = $stmt_cart_sell->fetchAll(PDO::FETCH_ASSOC);
 
 <body id="page-top">
 
-    <?php require_once('../User/alert.php') ?>
     <!-- Page Wrapper -->
     <div id="wrapper">
 
         <div id="menu"></div>
         <!-- Content Wrapper -->
         <div id="content-wrapper" class="d-flex flex-column">
-
             <!-- Main Content -->
             <div id="content">
-                <div id="sell">
-                    <div class="row ms-2 mt-5">
-                        <div class="col-5 input-group">
-                            <span class="input-group-text">รหัสสินค้า</span>
-                            <input class="form-control col-4" autofocus name="ID_Product" id="Select_ID_Product" placeholder="รหัสสินค้า...">
-                        </div>
-                    </div>
-                    <div class="row ms-2 mt-2">
-                        <div class="col-5 ms-2 mt-3 box_sell_product">
-                            <div class="row mb-4 mt-2 justify-content-center text-center">
-
-                                <div class="col-3 h4">รูป</div>
-                                <div class="col-4 h4 text-start">ชื่อสินค้า</div>
-                                <div class="col-2 h4">จำนวน</div>
-                                <div class="col-2 h4">ราคา</div>
-                                <div class="col-1 h4"></div>
-                            </div>
-                            <?php
-                            $total = 0;
-                            foreach ($result_cart_sell as $row_cart_sell) { ?>
-                                <div class="row justify-content-center text-center align-items-center mt-2 ">
-                                    <div class="col-3"> <img width="65" height="65" src="../Asset/img/<?php echo $row_cart_sell['IMG_Product']; ?>"></div>
-                                    <div class="col-4 text-start"> <?php echo $row_cart_sell['NAME_Product']; ?> </div>
-                                    <div class="col-2"> <?php echo $row_cart_sell['QTY']; ?> </div>
-                                    <div class="col-2"> <?php echo $row_cart_sell['QTY'] *  $row_cart_sell['PRICE_Product'] ?>.-บาท </div>
-                                    <div class="col-1 btn btn-danger btn-sm del_sell<?php echo $row_cart_sell['ID_Product']; ?>"><i class=" fas fa-trash-alt"></i></div>
-                                    <input type="hidden" id="ID_Cart<?php echo $row_cart_sell['ID_Cart']; ?>" value="<?php echo $row_cart_sell['ID_Cart']; ?>">
-                                    <input type="hidden" id="Cart_ID_Product<?php echo $row_cart_sell['ID_Cart']; ?>" value="<?php echo $row_cart_sell['ID_Product']; ?>">
-                                </div>
-                                <?php
-                                $sum = $row_cart_sell['QTY'] *  $row_cart_sell['PRICE_Product'];
-                                $total = $total + $sum;
-                                ?>
-
-                                <script>
-                                    // ปุ่มสินค้า
-                                    $('.del_sell<?php echo $row_cart_sell['ID_Product']; ?>').click(function() {
-                                        var ID_Cart = $('#ID_Cart<?php echo $row_cart_sell['ID_Cart']; ?>').val();
-                                        var Cart_ID_Product = $('#Cart_ID_Product<?php echo $row_cart_sell['ID_Cart']; ?>').val();
-
-                                        $.ajax({
-                                            url: "../sql/del.php",
-                                            method: "post",
-                                            data: {
-                                                action: 'del_sell',
-                                                ID_Cart: ID_Cart,
-                                                Cart_ID_Product: Cart_ID_Product,
-                                            },
-                                            success() {
-                                                $('#sell').load('sell.php');
-                                            }
-                                        });
-                                    });
-                                </script>
-                            <?php } ?>
-                        </div>
-                        <div class="col-2 text-center box_payment">
-                            <h4 class="text-start">ราคาทั้งหมด <?php echo $total; ?>.-บาท</h4>
-                            <button class="btn btn-success btn-lg col-12 confirm_sell">ชำระเงิน</button>
-                        </div>
-                    </div>
-
-                </div>
-
-
-
-
+                <div id="sell"></div>
             </div>
             <!-- End of Content Wrapper -->
         </div>
@@ -157,52 +89,12 @@ $result_cart_sell = $stmt_cart_sell->fetchAll(PDO::FETCH_ASSOC);
         <script src="js/demo/chart-pie-demo.js"></script>
         <!-- sweetalert2 -->
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
-
         <script>
             $(document).ready(function() {
                 $('#menu').load('menu.php');
-                $("#Select_ID_Product").focus();
-
-                $('#Select_ID_Product').change(function() {
-                    var ID_Product = $('#Select_ID_Product').val();
-                    console.log(ID_Product);
-
-                    $.ajax({
-                        url: "../sql/db_Add_cart_sell.php",
-                        method: "post",
-                        data: {
-                            ID_Product: ID_Product
-                        },
-                        success() {
-                            ID_Product = $('#Select_ID_Product').val("");
-
-                            $('#sell').load('sell.php');
-                        }
-                    });
-                });
-
-                $('.confirm_sell').click(function() {
-                    $.ajax({
-                        url: "../sql/confirm_sell.php",
-                        success() {
-                            $('#sell').load('sell.php');
-                            Swal.fire({
-                                title: 'ทำรายการสำเร็จ',
-                                icon: 'success',
-                                timer: 1000,
-                                showConfirmButton: false,
-
-                            })
-                        }
-                    });
-                });
-
+                $('#sell').load('sell_items.php');
             });
         </script>
-
-
-
 </body>
 
 </html>
