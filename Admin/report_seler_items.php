c081afca2a3a02f8bb0778e1fb5ec871cb046a72
MiNi <nitipoom42@gmail.com>
2021-09-13 00:46:41 +0700

    -

--- Admin/report_seler_items.php
+++ Admin/report_seler_items.php
@@ -12,7 +12,7 @@ JOIN oder_detail as od ON  o.ID_Oder = od.ID_Oder
 JOIN stock as s ON s.ID_Product = od.ID_Product
 JOIN type_product as tp ON tp.ID_Type_Product = s.TYPE_Product
 WHERE date(o.Oder_date) BETWEEN :date_start AND :date_end AND  o.oder_status >=3 AND s.Status_Product = 1
-GROUP BY date(o.Oder_date), o.oder_status ,s.ID_Product;
+GROUP BY date(o.Oder_date) ,s.ID_Product;
 
 ";
 $stmt_report_seler = $conn->prepare($sql_report_seler);
