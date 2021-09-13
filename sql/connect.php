26e7878b8ae63f43758e110dd30a4d3fbfceac54
MiNi <nitipoom42@gmail.com>
2021-09-13 15:51:02 +0700

    ตัด order สินค้าถ้าไม่โอนเงิน ภายใน 30 นาที

--- sql/connect.php
+++ sql/connect.php
@@ -1,23 +1,23 @@
   <?php
-    session_start();
-    date_default_timezone_set("Asia/Bangkok");
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "nongmindshop";
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    } catch (PDOException $e) {
-        echo "เชื่อมต่อฐานข้อมูลไม่สำเร็จ: " . $e->getMessage();
-    }
-    ?>
+  session_start();
+  date_default_timezone_set("Asia/Bangkok");
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "nongmindshop";
+  try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch (PDOException $e) {
+    echo "เชื่อมต่อฐานข้อมูลไม่สำเร็จ: " . $e->getMessage();
+  }
+  ?>
 
   <?php
-    function result($data)
-    {
-        echo ("<pre>");
-        print_r($data);
-        echo ("<pre>");
-    }
-    ?>
+  function result($data)
+  {
+    echo ("<pre>");
+    print_r($data);
+    echo ("<pre>");
+  }
+  ?>
--- sql/db_cancel_oder_timeout.php
+++ sql/db_cancel_oder_timeout.php
@@ -1,46 +1,63 @@
 <?php
 require_once('connect.php');
 
+$sql_oder_time_default = "SELECT * FROM oder";
+$stmt_oder_time_default  = $conn->prepare($sql_oder_time_default);
+$stmt_oder_time_default->execute();
+$result_oder_time_default  = $stmt_oder_time_default->fetchAll(PDO::FETCH_ASSOC);
 
-$sql_oder_time = "SELECT * FROM oder as o
-INNER JOIN  oder_detail as od ON o.ID_Oder = od.ID_Oder WHERE oder_status = 0";
-$stmt_oder_time = $conn->prepare($sql_oder_time);
-$stmt_oder_time->execute();
-$result_oder_time = $stmt_oder_time->fetchAll(PDO::FETCH_ASSOC);
+foreach ($result_oder_time_default as $row_time_default) {
+    $data = [
+        'ID_Oder' => $row_time_default['ID_Oder'],
+    ];
+    $sql_oder_time = "SELECT * FROM oder as o
+    INNER JOIN  oder_detail as od ON o.ID_Oder = od.ID_Oder WHERE oder_status = 0 AND o.ID_Oder=:ID_Oder";
+    $stmt_oder_time = $conn->prepare($sql_oder_time);
+    $stmt_oder_time->execute($data);
+    $result_oder_time = $stmt_oder_time->fetchAll(PDO::FETCH_ASSOC);
 
-foreach ($result_oder_time as $row_oder_time) {
+    foreach ($result_oder_time as $row_oder_time) {
+        $row_oder_time['Oder_date'];
+        $date_order_default = strtotime($row_oder_time['Oder_date'], 10);
+        $date_order_result = date("H:i:s", $date_order_default);
+        $Time_out = date("H:i:s", strtotime('+30 minutes', $date_order_default));
+        $date_now_oder = date("H:i:s");
 
-    $row_oder_time['Oder_date'];
-    echo $date_order =  date("H:i:s", strtotime($row_oder_time['Oder_date'], 10));
+        if ($date_now_oder > $Time_out) {
+            echo "เกินเวลา";
 
+            $data_ID_Oder = [
+                'ID_Oder' => $row_time_default['ID_Oder'],
+            ];
+            $sql_oder = "SELECT * FROM oder as o
+            INNER JOIN  oder_detail as od ON o.ID_Oder = od.ID_Oder
+            WHERE od.ID_Oder=:ID_Oder";
+            $stmt_oder = $conn->prepare($sql_oder);
+            $stmt_oder->execute($data_ID_Oder);
+            $result_oder = $stmt_oder->fetchAll(PDO::FETCH_ASSOC);
 
+            foreach ($result_oder as $row_oder) {
+                $data_Product = [
+                    'ID_Product' => $row_oder['ID_Product'],
+                    'QTY' => $row_oder['QTY'],
+                ];
+                $sql_update_product = "UPDATE stock SET QTY_Product=QTY_Product+:QTY WHERE ID_Product=:ID_Product";
+                $stmt_update_product = $conn->prepare($sql_update_product);
+                $stmt_update_product->execute($data_Product);
 
+                $sql_update_product_promotion = "UPDATE stock_promotion SET QTY_Product=QTY_Product+:QTY WHERE ID_Product_Promotion=:ID_Product";
+                $stmt_update_product_promotion = $conn->prepare($sql_update_product_promotion);
+                $stmt_update_product_promotion->execute($data_Product);
 
-    exit();
-    if ($date_order > $Time_out) {
-
-        $data_Product_time = [
-            'ID_Product' => $row_oder_time['ID_Product'],
-            'QTY' => $row_oder_time['QTY'],
-        ];
-        $sql_update_product_time = "UPDATE stock SET QTY_Product=QTY_Product+:QTY WHERE ID_Product=:ID_Product";
-        $stmt_update_product_time = $conn->prepare($sql_update_product_time);
-        $stmt_update_product_time->execute($data_Product_time);
-
-        $sql_update_product_promotion_time = "UPDATE stock_promotion SET QTY_Product=QTY_Product+:QTY WHERE ID_Product_Promotion=:ID_Product";
-        $stmt_update_product_promotion_time = $conn->prepare($sql_update_product_promotion_time);
-        $stmt_update_product_promotion_time->execute($data_Product_time);
-
-        $data_Oder_time = [
-            'ID_Oder' => $row_oder_time['ID_Oder'],
-        ];
-        // ลบข้อมูลตาราง oder_detail
-        $sql_cancel_oder_detail_time = "DELETE FROM oder_detail WHERE ID_Oder=:ID_Oder";
-        $stmt_cancel_oder_detail_time = $conn->prepare($sql_cancel_oder_detail_time);
-        $stmt_cancel_oder_detail_time->execute($data_Oder_time);
-        // ลบข้อมูลตาราง ตาราง oder
-        $sql_cancel_oder_time = "DELETE FROM oder WHERE ID_Oder=:ID_Oder";
-        $stmt_cancel_oder_time = $conn->prepare($sql_cancel_oder_time);
-        $stmt_cancel_oder_time->execute($data_Oder_time);
+                // ลบข้อมูลตาราง oder_detail
+                $sql_cancel_oder_detail = "DELETE FROM oder_detail WHERE ID_Oder=:ID_Oder";
+                $stmt_cancel_oder_detail = $conn->prepare($sql_cancel_oder_detail);
+                $stmt_cancel_oder_detail->execute($data_ID_Oder);
+                // ลบข้อมูลตาราง ตาราง oder
+                $sql_cancel_oder = "DELETE FROM oder WHERE ID_Oder=:ID_Oder";
+                $stmt_cancel_oder = $conn->prepare($sql_cancel_oder);
+                $stmt_cancel_oder->execute($data_ID_Oder);
+            }
+        }
     }
 }
